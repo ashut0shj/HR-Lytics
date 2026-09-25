ac2df8147fd4089e25d52103f90bdaf9d6641e0f
Shadan-malak <malakshadan2004@gmail.com>
2026-09-25 17:21:58 +0530

    commit

--- sql/01_oltp_schema.sql
+++ sql/01_oltp_schema.sql
@@ -8,8 +8,8 @@ CREATE TABLE IF NOT EXISTS departments (
 
 CREATE TABLE IF NOT EXISTS employees (
     employee_id      INT PRIMARY KEY,
-    first_name       VARCHAR(60)     NOT NULL,
-    last_name        VARCHAR(60)     NOT NULL,
+    first_name       VARCHAR(60)     NOT NULL CHECK (first_name REGEXP '^[A-Za-z]+$'),
+    last_name        VARCHAR(60)     NOT NULL CHECK (last_name REGEXP '^[A-Za-z]+$'),
     email             VARCHAR(150)    NOT NULL UNIQUE,
     gender           VARCHAR(10),
     age              INT,
@@ -64,6 +64,8 @@ CREATE TABLE IF NOT EXISTS assignments (
         FOREIGN KEY (employee_id) REFERENCES employees(employee_id),
     CONSTRAINT fk_assignment_project
         FOREIGN KEY (project_id) REFERENCES projects(project_id)
+        ON DELETE RESTRICT
+        ON UPDATE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS reviews (
